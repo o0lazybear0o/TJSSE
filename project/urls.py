892d7e56f3a085ca39cdc13bc8a5571d96494cfa
Iris Chen <o0lazybear0o@gmail.com>
2016-05-01 15:50:37 +0800

    实现了project的分页--iris

--- project/urls.py
+++ project/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.project_list_view,name='project_list'),
     url(r'^(?P<project_id>[0-9]+)/$', views.project_detail_view, name='project_detail'),
-    url(r'^search$',views.project_search_list_view,name='project_search_list'),
+    url(r'^search/$',views.project_search_list_view,name='project_search_list'),
 ]
